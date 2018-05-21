32fa70301f98208b0c73bdf3a126e9712237859c
Marvin Reinhart <reinhart.marvin@hotmail.de>
2018-05-21 10:30:30 +0200

    ADD: GMail func. for index.php form

--- assets/includes/layout/nav.php
+++ assets/includes/layout/nav.php
@@ -5,6 +5,7 @@ echo '
     <nav>
         <h2>Navigation</h2>
         <ul>
+            <li><a href="index.php">Home</a></li>
             <li><a href="mail.php">Mail</a></li>
             <li><a href="kalender.php">Kalender</a></li>
             <li><a href="todo.php">To Do</a></li>
