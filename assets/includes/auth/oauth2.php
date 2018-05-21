45f7972ccbc6bd123de5f44f0a3a5c52f028c740
Marvin Reinhart <reinhart.marvin@hotmail.de>
2018-05-21 12:52:54 +0200

    UPDATE: Latest working system /w Mail & Cal func.

--- assets/includes/auth/oauth2.php
+++ assets/includes/auth/oauth2.php
@@ -4,9 +4,11 @@ require_once 'assets/php-oauth2/vendor/autoload.php';
 session_start();
 
 $client = new Google_Client();
+$client->setApplicationName('Bridge.io');
 $client->setAuthConfig('assets/php-oauth2/client_secrets.json');
 $client->addScope('https://mail.google.com/');
 $client->addScope('https://www.googleapis.com/auth/drive');
 $client->addScope('https://www.googleapis.com/auth/calendar');
+$client->setAccessType('offline');
 
 ?>
